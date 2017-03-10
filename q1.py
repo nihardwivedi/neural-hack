2e8f8865b124a980e5831845a568d8591b4515eb
--Nihar Dwivedi-- <thetimetravelerokarin@gmail.com>
2017-03-10 06:26:02 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# Traverse all of the files in the given directory and move
+# them into subfolders based on their extension type.
+import os
+import sys
+path1 = os.getcwd()
+print path1
+if __name__ == '__main__':
+    # Define the folder names for each extension type here
+    folderNames = {".txt" : "txt",
+                   ".csv" : "csv",
+                   ".mp3" : "mp3"}
+
+     
+    # NOTE: Assuming files in current folder
+    directory = "./"
+    files = os.listdir(directory)
+
+    # Traverse all of the files
+    for filename in files:
+        # Grab the extension of the file
+        extension = os.path.splitext(filename)[1]
+        
+        if extension in folderNames:
+            # Grab the folder where this file should be stored
+            folder = os.path.join(directory, folderNames[extension])
+
+            # Create the folder if it does not already exist
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+                
+            # Move the file into the folder
+            os.rename(os.path.join(directory, filename),
+            os.path.join(folder, filename))
